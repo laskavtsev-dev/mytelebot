0f5d1d1b98ad3d863a019174bb4040dbd21c56c7
laskavtsev-dev <152629202+laskavtsev-dev@users.noreply.github.com>
2024-04-01 09:08:28 +0000

    Workspaces Job

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"log"
-	"os"
-	"time"
-
-	tele "gopkg.in/telebot.v3"
-)
+import "gopkg.in/telebot.v3/cmd"
 
 func main() {
-	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-	}
-
-	b, err := tele.NewBot(pref)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
-	})
-
-	b.Start()
-
+	cmd.Execute()
 }
