b1d5abbaa9430adf1541fac3c8da07a00a061eff
laskavtsev-dev <152629202+laskavtsev-dev@users.noreply.github.com>
2024-04-01 12:25:33 +0000

    puuuuush

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,39 +1,49 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Oleksandr Laskavtsev <las@agrain.ua>
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
 )
 
-// kbotCmd represents the kbot command
-var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var (
+	//TELETOKEN BOT
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+var kbotCMD = &cobra.Command{
+	Use:     "kbot",
+	Aliases: []string{"run"},
+	Short:   "Short description",
+	Long:    "Long description",
+
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+
+		fmt.Printf("kbot %s started", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+
+			return err
+		})
+
+		kbot.Start()
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(kbotCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
