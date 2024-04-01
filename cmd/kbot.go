4fc684b90d69015b7b1498b25b8d405fdb8c8174
laskavtsev-dev <152629202+laskavtsev-dev@users.noreply.github.com>
2024-04-01 10:04:17 +0000

    bla bla

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"os"
 	"time"
-	
+
 	"github.com/spf13/cobra"
 	telebot "gopkg.in/telebot.v3"
 )
@@ -18,30 +18,30 @@ var (
 )
 
 var kbotCMD = &cobra.Command{
-	Use: "kbot",
+	Use:   "kbot",
 	Short: "Short description",
-	Long: "Long description",
-	
-	Run: func(cmd *cobra.Command, args []string){
-	
+	Long:  "Long description",
+
+	Run: func(cmd *cobra.Command, args []string) {
+
 		fmt.Printf("kbot %s started", appVersion)
 		kbot, err := telebot.NewBot(telebot.Settings{
-			URL: "",
-			Token: TeleToken,
+			URL:    "",
+			Token:  TeleToken,
 			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 		})
-			
+
 		if err != nil {
 			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
 		}
-		
+
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			log.Pring(m.Message().Payload, mText())
-			
+
 			return err
 		})
-		
+
 		kbot.Start()
 	},
 }
